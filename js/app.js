b7c6343d4f90b014def04179cbba249aae2e0f85
Rafael Kauch <shuuwolf_second@outlook.com>
2022-01-27 09:16:36 -0300

    finalização do projeto.

--- js/app.js
+++ js/app.js
@@ -2,3 +2,7 @@ $('nav a').hover(function(){
     $('nav li').removeClass('selected');
     $(this).parent().addClass('selected');
 })
+
+$('.mobile h3').on('click', function(){
+    $('.nav-mobile').slideToggle();
+});
